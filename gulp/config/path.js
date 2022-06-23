f8fbaa90c2d82e7ebfff3317eb4a99551070157a
Yurii <brechkoio@gmail.com>
2022-06-23 13:01:50 +0300

    add copy task

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -3,3 +3,18 @@ const rootFolder = nodePath.basename(nodePath.resolve());
 
 const buildFolder = './dist';
 const srcFolder = './src';
+
+export const path = {
+    build: {
+        files: `${buildFolder}/files/`,
+    },
+    src: {
+        files: `${srcFolder}/files/**/*.*`,
+    },
+    watch: {},
+    clean: buildFolder,
+    buildFolder: buildFolder,
+    srcFolder: srcFolder,
+    rootFolder: rootFolder,
+    ftp: ``
+}
---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+import gulp from "gulp";
+
+import { path } from "./gulp/config/path";
+
+global.app = {
+    path: path,
+    gulp: gulp,
+}
