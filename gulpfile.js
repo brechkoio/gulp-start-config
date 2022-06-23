578d695bfd6ca253f77471eebfa2a8ed7434467c
Yurii <brechkoio@gmail.com>
2022-06-23 13:20:45 +0300

    add reset task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 import gulp from "gulp";
 
-import { path } from "./gulp/config/path";
+import { path } from "./gulp/config/path.js";
 
 global.app = {
     path: path,
     gulp: gulp,
 }
+
+import { copy } from "./gulp/tasks/copy.js";
+
+function watcher() {
+    gulp.watch(path.watch.files, copy);
+}
+
+const dev = gulp.series(copy, watcher);
+
+gulp.task('default', dev);
