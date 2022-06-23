f5b8b10da4597b5d89917d0e60bf8066e43e6bf0
Yurii <brechkoio@gmail.com>
2022-06-23 15:08:17 +0300

    add html task

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,16 @@ global.app = {
 }
 
 import { copy } from "./gulp/tasks/copy.js";
+import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
 
 function watcher() {
     gulp.watch(path.watch.files, copy);
+    gulp.watch(path.watch.html, html)
 }
 
-const dev = gulp.series(copy, watcher);
+const mainTasks = gulp.parallel(copy, html);
+
+const dev = gulp.series(reset, mainTasks, watcher);
 
 gulp.task('default', dev);
